d2f5b17ce83075751e86b1447ec4e822997794f8
Brad <bmlegge12.gmail.com>
2025-09-03 19:42:48 -0400

    added start function. added json file. added room_exits function. added valid_room function

--- main.py
+++ main.py
@@ -12,23 +12,32 @@ def read_file():
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+rooms = read_file()
+lst = []
+
 def main():
-    data = read_file()
-    lst = []
     current_room = "start_room"
+    
+    game_start()
+    room_exits(current_room)
 
-    print(data["start_room"]["description"])
-    for i in data["start_room"]["exits"]:
-        lst.append(i)
-    print(f"These are the exits: {" ".join(lst)}")
-    print("What would you like to do?")
+def game_start():
+    print("Welcome to The Game! Travel from room to room collecting loot and find the key to unlock "
+    "the boss room. If you are brave enough fight the boss and collect the ultimate prize!")
+
+def room_exits(current_room):        
+    print(rooms[current_room]["description"])
+    
+    print(f"There are the exits: {" ".join(rooms[current_room]["exits"].keys())}")
+    print("Where would you like to go?")
     user_input = input("==> ")
+    valid_room(user_input, current_room)
 
-    if user_input in data[current_room]["exits"]:
-        current_room = data[current_room]["exits"][user_input]
-        print(f"You are in {current_room}")
-    else: print("Outside")
-
+def valid_room(user_input, current_room):
+    if user_input in rooms[current_room]["exits"]:
+        current_room = rooms[current_room]["exits"][user_input]
+        room_exits(current_room)
+        
 
 if __name__ == "__main__":
     main()
