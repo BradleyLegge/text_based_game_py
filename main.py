fd02a9ddce5fc79c7a1468c7103f26064c17888e
BradleyLegge <bmlegge12@gmail.com>
2025-09-08 17:11:38 -0400

    added loot.json file. added search function to allow player to search and add items to inventory

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import json
 import random
 from models.player import Person
 from models.mobs import Goblin, Kobald, Orc
-file_path = "rooms.json"
 
-def read_file():
+
+def read_file(file_path):
     try:
         with open(file_path, "r") as file:
             return json.load(file)
@@ -15,7 +15,8 @@ def read_file():
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-rooms = read_file()
+rooms = read_file("rooms.json")
+loot = read_file("loot.json")
 
 def game_start():
     print("Welcome to The Game!")
@@ -24,34 +25,37 @@ def game_start():
     player_name = input("Enter your character name: ==> ")
     player = Person(player_name)
     current_room = "start_room"
+    game_running = True
 
     menu()
 
-    while player.health > 0: #or boss.health > 0
+    while game_running and player.health > 0: #or boss.health > 0
         print("What would you like to do? ('m' for menu)")
-        user_input = input("==> ")
+        user_input = input("==> ").lower().strip()
         print(" ")
         
-        if user_input.lower() == 'move':
+        if user_input == 'move':
             current_room = display_room(current_room)
-        elif user_input.lower()== 'search':
-            search()
-        elif user_input.lower() == 'fight':
+        elif user_input== 'search':
+            search(player)
+        elif user_input == 'fight':
             make_attack(player)
-        elif user_input.lower() == 'menu' or 'm':
+        elif user_input in ('menu', 'm'):
             menu()
-        elif user_input.lower() == 'status':
+        elif user_input == 'status':
             player_status(player)
-        elif user_input.lower() == 'kill':
-            kill_switch(player)
+        elif user_input == 'exit':
+            game_running = False
         else: print("You entered a wrong command!")
 
 def menu():
     print("To move from room to room type: 'move'")
     print("To search the room type: 'search'")
     print("To fight a mob you encounter type: 'fight'")
+    print("To try to loot type: 'loot'")
     print("To view the menu type: 'menu'")
     print("To view your player status type: 'status'")
+    print("To exit the game type: 'exit")
 
 def display_room(current_room):
     print(rooms[current_room]["description"])
@@ -67,14 +71,13 @@ def display_room(current_room):
 
     return current_room
 
-def search():
-    room_search = random.randint(1, 3)
-    if room_search == 1:
-        print("There is something moving about in this room!")
-    elif room_search == 2:
-        print("You see something shiny laying on the desk!")
-    elif room_search == 3:
-        print("The room is completely empty")
+def search(player):
+    # room_search = random.randint(1, 1)
+    # if room_search == 1:
+    for i in loot:
+        if i["name"] == "key":
+            loot_item = i["name"]
+    player.add_item(loot_item)
 
 def make_attack(player):
     mob_types = [Goblin, Kobald, Orc]
@@ -115,6 +118,7 @@ def player_attack(player, mob):
         if attack_roll >= mob.ac:
             mob.take_damage(player.base_attack)
         else: print("Attack missed!\n")
+
 
 def kill_switch(player):
     player.health = 0
